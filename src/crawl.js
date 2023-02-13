113affa051d7f85879fee0faa9d112436fb52891
Samee Vohra <samee.vohra@gmail.com>
2023-02-13 13:48:52 -0800

    update file structure and add eslint/prettier

--- src/crawl.js
+++ src/crawl.js
@@ -1,24 +1,23 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
 
 const normalizeURL = (urlString) => {
   const urlObj = new URL(urlString);
 
   let fullPath = `${urlObj.hostname}${urlObj.pathname}`;
-  if(fullPath.length && fullPath.slice(-1) === '/') {
+  if (fullPath.length && fullPath.slice(-1) === "/") {
     return fullPath.slice(0, -1);
   }
-  
-  return fullPath;
-}
 
+  return fullPath;
+};
 
 const getURLsFromHTML = (htmlBody, baseURL) => {
   const urls = [];
   const dom = new JSDOM(htmlBody);
-  const linkElements = dom.window.document.querySelectorAll('a');
+  const linkElements = dom.window.document.querySelectorAll("a");
 
-  linkElements.forEach( link => {
-    if(link.href[0] === '/') {
+  linkElements.forEach((link) => {
+    if (link.href[0] === "/") {
       //relative
       try {
         const urlObj = new URL(`${baseURL}${link.href}`);
@@ -30,25 +29,24 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
       //absolute
       try {
         const urlObj = new URL(link.href);
-        urls.push(urlObj.href)
+        urls.push(urlObj.href);
       } catch (err) {
-        console.log(`${link.href} is not a valid URL: ${err.message}`)
+        console.log(`${link.href} is not a valid URL: ${err.message}`);
       }
     }
-  })
+  });
   return urls;
-}
+};
 
 async function crawlPage(baseURL, currentURL, pages) {
-
   const baseURLObj = new URL(baseURL);
   const currentURLObj = new URL(currentURL);
 
-  if(baseURLObj.hostname !== currentURLObj.hostname) return pages;
+  if (baseURLObj.hostname !== currentURLObj.hostname) return pages;
 
   const normalizedURL = normalizeURL(currentURL);
 
-  if(normalizedURL in pages) {
+  if (normalizedURL in pages) {
     pages[normalizedURL]++;
     return pages;
   } else {
@@ -56,41 +54,39 @@ async function crawlPage(baseURL, currentURL, pages) {
   }
 
   // console.log(pages)
-  console.log(`Actively crawling ${currentURL}.`)
+  console.log(`Actively crawling ${currentURL}.`);
 
   try {
-    const res = await fetch(currentURL)
-    if(res.status > 399) {
+    const res = await fetch(currentURL);
+    if (res.status > 399) {
       // throw new Error(`Received HTTP Error. Status Code: ${res.status}`)
-      console.log(`Received HTTP Error. Status Code: ${res.status}`)
+      console.log(`Received HTTP Error. Status Code: ${res.status}`);
       return pages;
     }
 
-    const contentType = res.headers.get('content-type');
-    if(!contentType.includes('text/html')) {
+    const contentType = res.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
       // throw new Error(`Received non-html response`);
-      console.log(`Received non-html response from ${currentURL}.`)
+      console.log(`Received non-html response from ${currentURL}.`);
       return pages;
     }
 
     const htmlBody = await res.text();
     const nextURLs = getURLsFromHTML(htmlBody, baseURL);
 
-    for(const nextURL of nextURLs) {
-      pages = await crawlPage(baseURL, nextURL, pages)
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
     }
 
     return pages;
-
   } catch (err) {
-    console.log(`Error in fetching ${currentURL}: ${err.message}`)
+    console.log(`Error in fetching ${currentURL}: ${err.message}`);
     return pages;
   }
 }
 
-
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
-  crawlPage
-}
+  crawlPage,
+};
---
+++ src/main.js
@@ -0,0 +1,20 @@
+const { crawlPage } = require("./crawl.js");
+const { printReport } = require("./report.js");
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("No website provided.");
+    process.exit(1);
+  } else if (process.argv.length > 3) {
+    console.log("Too many arguments provided.");
+    process.exit(1);
+  } else {
+    const baseURL = process.argv[2];
+    console.log(`Starting web crawl of ${baseURL}.`);
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+    printReport(pages);
+  }
+}
+
+main();
